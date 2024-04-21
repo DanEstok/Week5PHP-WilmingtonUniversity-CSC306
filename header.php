74c77bf9f0b47dd3e14fec3b23cff7df55113365
DanEstok <destok001@my.wilmu.edu>
2024-04-21 16:48:17 -0400

    Update CSS styles in style.css

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doctor Who</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="animation-container">
+        <canvas id="animation-canvas"></canvas>
+    </div>
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="index.php">Doctor Who</a>
+            </div>
+            <ul>
+                <li><a href="addPatient.php">Add Patient</a></li>
+                <li><a href="getPatient.php">View Patients</a></li>
+                <li><a href="getResults.php">View Records</a></li>
+            </ul>
+        </nav>
+    </header>
